59d623531efbfdda76720ecbbfeb2e35aa5f3dfd
Skalmanleif <isak.thyrel@elev.ga.ntig.se>
2020-10-09 11:09:23 +0200

    Hello

---
+++ src/Helloworld.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Helloworld {
+    public static void main (String []args){
+
+        String hej = "Hello world"
+        JOptionPane.showMessageDialog(null, hej);
+    }
+}
